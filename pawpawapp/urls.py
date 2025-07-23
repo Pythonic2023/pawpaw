1b07155461f5cdf5ef5eda6bf6f3744408330d18
Pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2025-07-23 09:10:04 -0400

    Added basic templates for my web pages which inherit from base.html

---
+++ pawpawapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("cart/", views.cart, name="cart"),
+    path("products/", views.products, name="products"),
+    path("news/", views.news, name="news"),
+    path("contacts", views.contact, name="contact"),
+]
---
+++ readme.md
@@ -0,0 +1,5 @@
+PAWPAW
+
+This django application will be a template for pet businesses to use and advertise their various services.
+
+This will be updated with more content as the project grows. 
