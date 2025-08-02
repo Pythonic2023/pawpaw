2f2d8fdac17eaff0c274b02ee887f99e059736ef
Pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2025-08-02 13:26:18 -0400

    Added account.css to make error messages appear red
    Added non_field_errors to display user already taken and similar messages.

--- pawpawapp/views.py
+++ pawpawapp/views.py
@@ -41,8 +41,8 @@ def account(request):
         form = AuthenticationForm()
         signup_form = SignUpForm(prefix="user_signup")
         context = {
-            "signupform": signup_form,
             "signinform": form,
+            "signupform": signup_form,
         }
         return render(request, "account.html", context)
 
@@ -66,22 +66,27 @@ def signup(request):
 
 
 def signin(request):
-    form = AuthenticationForm(request, data=request.POST)
-    if form.is_valid():
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user = authenticate(request, username=username, password=password)
-        print(f"{user} AUTHENTICATED")
-        if user is not None:
-            login(request, user)
-            return HttpResponse(f"logged in as {user}")
+    user_signin = AuthenticationForm(request, data=request.POST)
+    print(f"Form type: {request.POST.get('form_type')}")
+    if request.POST.get('form_type') == 'signin':
+        if user_signin.is_valid():
+            username = user_signin.cleaned_data['username']
+            password = user_signin.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            print(f"{user} AUTHENTICATED")
+            if user is not None:
+                login(request, user)
+                return HttpResponse(f"logged in as {user}")
+            else:
+                user_signup = SignUpForm()
+                user_signin = AuthenticationForm()
+                return render(request, "account.html", {'signupform': user_signup, 'signinform': user_signin})
         else:
-            user_signup = SignUpForm(request.POST, prefix="user_signup")
-            user_signin = AuthenticationForm()
-            return render(request, "account.html", {'signupform': user_signup, 'signinform': user_signin})
+            user_signup = SignUpForm()
+            return render(request, "account.html", {'signinform': user_signin, 'signupform': user_signup})
     else:
-        user_signup = SignUpForm(request.POST, prefix="user_signup")
-        user_signin = AuthenticationForm()
+        user_signup = SignUpForm()
+        user_signin = AuthenticationForm(request.POST, prefix=user_signin)
         return render(request, "account.html", {'signupform': user_signup, 'signinform': user_signin})
 
 
