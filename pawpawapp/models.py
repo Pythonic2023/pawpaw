87fe61879d4f1f6d7de8fabc532717042ebd2a54
Pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2025-08-03 09:15:18 -0400

    Added Cart and CartItem model

--- pawpawapp/models.py
+++ pawpawapp/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 from django.db.models import OneToOneField
+from django.contrib.auth.models import User
+
+class Cart(models.Model):
+    user = OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}"
 
 
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.item_name}"
